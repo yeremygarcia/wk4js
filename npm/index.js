ad7be47d8d4df6c69e8cdc2cf47a2cf443fedd04
yeremygarcia <jeremyisgarcia@gmail.com>
2023-04-25 14:16:32 -0400

    1

---
+++ npm/index.js
@@ -0,0 +1,11 @@
+//const Moment = require("moment");
+
+// const a = new Moment();
+// console.log(a.format("h:mm:ss a"));
+
+// logs the current time
+
+const Moment = require('moment');
+
+const a = new Moment();
+console.log(a.format('h:mm:ss a')); //logs the current time
